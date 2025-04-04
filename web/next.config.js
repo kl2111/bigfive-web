14917c77d60e1d5143fb0fe40e1fcaee3e9f88a2
KL2101 <JasonXlog@163.com>
2025-04-04 20:08:51 +0800

    fix: remove i18n config to enable next-intl middleware

--- web/next.config.js
+++ web/next.config.js
@@ -5,10 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'zh', 'de'], // 这里列出你支持的语言
-    defaultLocale: 'zh',         // 设置默认语言
-  },
 };
 
 module.exports = withContentlayer(withNextIntl(nextConfig));
