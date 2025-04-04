9d0689c8e732022f36d9b7f22e76f794aec95b8c
KL2101 <JasonXlog@163.com>
2025-04-04 19:50:08 +0800

    Update i18n configuration for automatic locale redirection

--- web/next.config.js
+++ web/next.config.js
@@ -4,6 +4,11 @@ const { withContentlayer } = require('next-contentlayer');
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'zh', 'de'], // 这里列出你支持的语言
+    defaultLocale: 'zh',         // 设置默认语言
+  },
+};
 
 module.exports = withContentlayer(withNextIntl(nextConfig));
